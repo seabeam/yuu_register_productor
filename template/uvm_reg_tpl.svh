22bbb283ffd1c5f48aa7e5f4541061e35dee403f
seabeam <seabeam@yahoo.com>
2019-02-18 19:23:47 +0800

    template bug fixed

--- template/uvm_reg_tpl.svh
+++ template/uvm_reg_tpl.svh
@@ -6,17 +6,20 @@ class reg_{{ module }}_{{ reg.name }} extends uvm_reg;
   rand uvm_reg_field {{ field.name }};
     {% endfor %}
 
-  function void new(string name="reg_{{ module }}_{{ reg.name }}");
-    super.new(name);
+  function new(string name="reg_{{ module }}_{{ reg.name }}",
+               int unsigned n_bits,
+               int has_coverage
+               );
+    super.new(name, n_bits, has_coverage);
   endfunction : new
 
   virtual function void build();
     {% for fkey, field in reg %}
     
     {{ field.name }} = new("{{ field.name }}");
-    {{ field.name }}.configure(.parent(this), .size({{ field.size }}),
-                    .access("{{ field.access.upper() }}"), .has_reset(1), .is_rand(0),
-                    .indevidually_access(0));
+    {{ field.name }}.configure(.parent(this), .size({{ field.size }}), .lsb_pos({{ field.lsb_pos }}),
+                               .access("{{ field.access.upper() }}"), .volatile(0), .reset({{ field.reset }}),
+                               .has_reset(1), .is_rand(0), .individually_accessible(0));
     {% endfor %}
   endfunction : build
 endclass : reg_{{ module }}_{{ reg.name }}
